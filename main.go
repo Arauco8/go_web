8b1e8098e6a71ee783016bb08acf7fb60edde923
Maximiliano Leiva <apumaleiva@gmail.com>
2025-03-30 18:05:53 -0300

    create GetAll

--- main.go
+++ main.go
@@ -6,15 +6,46 @@ import (
 	"net/http"
 )
 
+type User struct {
+	ID        uint64 `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+}
+
+var users []User
+
+func init() {
+	users = []User{{
+		ID:        1,
+		FirstName: "John",
+		LastName:  "Doe",
+		Email:     "john_doe@hotmail.com",
+	},
+		{
+			ID:        2,
+			FirstName: "Jane",
+			LastName:  "Smith",
+			Email:     "jane_smith@gmail.com",
+		},
+		{
+			ID:        3,
+			FirstName: "Alice",
+			LastName:  "Johnson",
+			Email:     "alice_johnson",
+		}}
+}
+
 type Response struct {
 	Status  int    `json:"status"`
 	Message string `json:"message"`
+	Data    []User `json:"data,omitempty"`
 }
 
 func UserServer(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		handleGet(w)
+		GetAllUsers(w)
 	case http.MethodPost:
 		handlePost(w)
 	default:
@@ -27,6 +58,27 @@ func handleGet(w http.ResponseWriter) {
 	response := Response{
 		Status:  http.StatusOK,
 		Message: "success in GET",
+	}
+	json.NewEncoder(w).Encode(response)
+}
+
+func GetAllUsers(w http.ResponseWriter) {
+	DataResponse(w, http.StatusOK, users)
+}
+
+func DataResponse(w http.ResponseWriter, status int, users interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	var userData []User
+	if u, ok := users.([]User); ok {
+		userData = u
+	}
+
+	response := Response{
+		Status:  status,
+		Message: "success",
+		Data:    userData,
 	}
 	json.NewEncoder(w).Encode(response)
 }
