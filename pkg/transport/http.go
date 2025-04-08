411742cc0b10173eb24662343d49237c68726d7c
Maximiliano Leiva <apumaleiva@gmail.com>
2025-04-08 00:31:39 -0300

    transport package

---
+++ pkg/transport/http.go
@@ -0,0 +1,55 @@
+package transport
+
+import (
+	"context"
+	"net/http"
+)
+
+type Transport interface {
+	Server(
+		enpoint Endpoint,
+		decode func(ctx context.Context, r *http.Request) (interface{}, error),
+		encode func(ctx context.Context, w http.ResponseWriter, response interface{}) error,
+		encodeError func(ctx context.Context, w http.ResponseWriter, err error) error,
+	)
+}
+
+type Endpoint func(ctx context.Context, request interface{}) (interface{}, error) // Endpoint is a function that takes a context and a request, and returns a response and an error
+
+type transport struct {
+	w   http.ResponseWriter
+	r   *http.Request
+	ctx context.Context
+}
+
+func NewTransport(w http.ResponseWriter, r *http.Request, ctx context.Context) Transport {
+	return &transport{
+		w:   w,
+		r:   r,
+		ctx: ctx,
+	}
+}
+
+func (t *transport) Server(
+	enpoint Endpoint,
+	decode func(ctx context.Context, r *http.Request) (interface{}, error),
+	encode func(ctx context.Context, w http.ResponseWriter, response interface{}) error,
+	encodeError func(ctx context.Context, w http.ResponseWriter, err error) error,
+) {
+	request, err := decode(t.ctx, t.r)
+	if err != nil {
+		encodeError(t.ctx, t.w, err)
+		return
+	}
+
+	response, err := enpoint(t.ctx, request)
+	if err != nil {
+		encodeError(t.ctx, t.w, err)
+		return
+	}
+
+	if err := encode(t.ctx, t.w, response); err != nil {
+		encodeError(t.ctx, t.w, err)
+		return
+	}
+}
